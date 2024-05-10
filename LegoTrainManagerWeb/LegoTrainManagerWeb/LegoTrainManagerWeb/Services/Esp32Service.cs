5927591e896d63a18de8a7c04e9ac34494b91134
Kevin <kevHTW@gmx.de>
2024-05-10 13:56:49 +0200

    add slider and try catch

--- LegoTrainManagerWeb/LegoTrainManagerWeb/LegoTrainManagerWeb/Services/Esp32Service.cs
+++ LegoTrainManagerWeb/LegoTrainManagerWeb/LegoTrainManagerWeb/Services/Esp32Service.cs
@@ -7,7 +7,8 @@ namespace LegoTrainManagerWeb.Services
 {
     public class Esp32Service : IEsp32Service
     {
-        public const string Address = "http://192.168.2.141/";
+        public const string IPAdress = "192.168.2.141";
+        public const string URL = $"http://{IPAdress}/";
 
         private readonly HttpClient _httpClient;
         private readonly ILogger<Esp32Service> _logger;
@@ -15,52 +16,92 @@ namespace LegoTrainManagerWeb.Services
         public Esp32Service(HttpClient client, ILogger<Esp32Service> logger)
         {
             _httpClient = client;
-            _httpClient.BaseAddress = new Uri(Address);
+            _httpClient.BaseAddress = new Uri(URL);
             _logger = logger;
         }
 
         public async Task<string> GetRoot()
         {
-            var request = await _httpClient.GetStringAsync("/");
-            _logger.LogInformation($"request msg: {request}");
-            return request;
+            try
+            {
+                var request = await _httpClient.GetStringAsync("/");
+                _logger.LogInformation($"request msg: {request}");
+                return request;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception msg: {e}");
+                return e.Message;
+            }
         }
 
         public async Task<string> GetCurrentTrainState()
         {
-            var request = await _httpClient.GetStringAsync("/currentTrainState");
-            _logger.LogInformation($"request msg: {request}");
-            return request;
+            try
+            {
+                var request = await _httpClient.GetStringAsync("/currentTrainState");
+                _logger.LogInformation($"request msg: {request}");
+                return request;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception msg: {e}");
+                return e.Message;
+            }
         }
 
         public async Task<string> StopTrain()
         {
-            var request = await _httpClient.GetStringAsync("/stop");
-            _logger.LogInformation($"request msg: {request}");
-            return request;
+            try
+            {
+                var request = await _httpClient.GetStringAsync("/stop");
+                _logger.LogInformation($"request msg: {request}");
+                return request;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception msg: {e}");
+                return e.Message;
+            }
         }
 
         public async Task<string> SetSpeed(int speed = 15)
         {
-            var url = $"speed?speed={speed}";
-            using var content = new StringContent("", Encoding.UTF8, "text/plain");
-            var request = await _httpClient.PostAsync(url, content);
-            _logger.LogInformation($"request msg: {request}");
-            return request.IsSuccessStatusCode ? "Speed Update" : "Speed Error";
+            try
+            {
+                var url = $"speed?speed={speed}";
+                using var content = new StringContent("", Encoding.UTF8, "text/plain");
+                var request = await _httpClient.PostAsync(url, content);
+                _logger.LogInformation($"request msg: {request}");
+                return request.IsSuccessStatusCode ? "Speed Update" : "Speed Error";
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception msg: {e}");
+                return e.Message;
+            }
         }
 
         public async Task<string> SetRGB(byte r, byte g, byte b)
         {
-            var content = new FormUrlEncodedContent(new[]
+            try
             {
-                new KeyValuePair<string, string>("r", r.ToString()),
-                new KeyValuePair<string, string>("g", g.ToString()),
-                new KeyValuePair<string, string>("b", b.ToString())
-            });
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("r", r.ToString()),
+                    new KeyValuePair<string, string>("g", g.ToString()),
+                    new KeyValuePair<string, string>("b", b.ToString())
+                });
 
-            var request = await _httpClient.PostAsync($"/setRGB", content);
-            _logger.LogInformation($"request msg: {request}");
-            return request.IsSuccessStatusCode ? "Update LED" : "Error LED";
+                var request = await _httpClient.PostAsync($"/setRGB", content);
+                _logger.LogInformation($"request msg: {request}");
+                return request.IsSuccessStatusCode ? "Update LED" : "Error LED";
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception msg: {e}");
+                return e.Message;
+            }
         }
     }
 }
--- LegoTrainManagerWeb/LegoTrainManagerWeb/LegoTrainManagerWeb/Services/IEsp32Service.cs
+++ LegoTrainManagerWeb/LegoTrainManagerWeb/LegoTrainManagerWeb/Services/IEsp32Service.cs
@@ -4,6 +4,7 @@ namespace LegoTrainManagerWeb.Services
 {
     public interface IEsp32Service
     {
+
         public Task<string> GetRoot();
         public Task<string> GetCurrentTrainState();
         public Task<string> StopTrain();
